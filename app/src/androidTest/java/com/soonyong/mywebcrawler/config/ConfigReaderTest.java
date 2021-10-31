734bb65b5178787d678a382b4227d6c51d0e241f
e406hsy <e406hsy@gmail.com>
2021-10-31 16:51:03 +0900

    feat: add config reader implementation

---
+++ app/src/androidTest/java/com/soonyong/mywebcrawler/config/ConfigReaderTest.java
@@ -0,0 +1,53 @@
+package com.soonyong.mywebcrawler.config;
+
+
+import android.content.Context;
+import android.util.Log;
+
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.platform.app.InstrumentationRegistry;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.io.IOException;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+@RunWith(AndroidJUnit4.class)
+public class ConfigReaderTest {
+
+    ConfigReaderImpl configReader;
+
+    @Before
+    public void setUp() throws Exception {
+        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
+        configReader = new ConfigReaderImpl(appContext, "test.json");
+    }
+
+
+    @Test
+    public void testGetCrawlConfig() throws IOException {
+        configReader.getCrawlConfig();
+    }
+
+    @Test
+    public void testSetCrawlConfig() throws IOException {
+        CrawlConfig crawlConfig = new CrawlConfig(List.of(CrawlConfig.Target.builder()
+                .active(false)
+                .interval(1000)
+                .targetXPath("test")
+                .url("http://www.example.com")
+                .build()));
+        this.configReader.setCrawlConfig(crawlConfig);
+
+        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
+        ConfigReader configReader = new ConfigReaderImpl(appContext, "test.json");
+        CrawlConfig savedCrawlConfig = configReader.getCrawlConfig();
+        assertEquals(savedCrawlConfig.getTargets().size(), crawlConfig.getTargets().size());
+        assertTrue(savedCrawlConfig.getTargets().containsAll(crawlConfig.getTargets()));
+    }
+}
--- app/src/test/java/com/soonyong/mywebcrawler/config/ConfigReaderTest.java
+++
@@ -1,7 +0,0 @@
-package com.soonyong.mywebcrawler.config;
-
-import junit.framework.TestCase;
-
-public class ConfigReaderTest extends TestCase {
-
-}
